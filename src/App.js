7e35ac8c81d62c159373141788a505bae4b309df
jeshua123 <inv.estevez@gmail.com>
2023-09-30 06:29:17 -0300

    firt commit 30-9-2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Prueba tecnica</h2>
       </header>
-    </div>
+      <table>
+
+        <tbody>
+          <tr >
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Pais</th>
+            <th>Acciones</th>
+          </tr>
+        </tbody>
+      </table>
+    </div >
   );
 }
 
