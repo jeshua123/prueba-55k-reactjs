6be4acb36e120b121947d68313f3e3182685190e
jeshua123 <inv.estevez@gmail.com>
2023-10-12 06:29:14 -0300

    other updared

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    fetch("https://randomuser.me/")
-      .then((response) => response.json())
-      .then((json) => console.log(json))
+    fetch('https://randomuser.me/api/?results=100')
+      .then(res => res.json())
+      .then(res => {
 
-  }, []);
+        setUsers(res.results)
+
+      })
+
+
+
+  }, [])
+
+  console.log(users.gender)
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Prueba tecnica</h2>
-      </header>
-      <table>
 
-        <tbody>
-          <tr >
-            <th>Foto</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Pais</th>
-            <th>Acciones</th>
-          </tr>
-          <tr>
-            <td>imagen </td>
-            <td>juan</td>
-            <td>perez</td>
-            <td>chile</td>
-            <td>5</td>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h2>Prueba tecnica</h2>
+          <button>Colorear Files</button>
+          <button>ordenar por pais</button>
+          <button>resetear estado</button>
+        </header>
+        <table>
 
-          </tr>
-        </tbody>
+          <tbody>
+            <tr >
+              <th>Foto</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Pais</th>
+              <th>Acciones</th>
+            </tr>
 
-      </table>
-    </div >
+            {users.map((user, index) => {
+
+              console.log(user.picture.thumbnail)
+
+              return (
+                <tr key={index}>
+                  <td><img src={user.picture.thumbnail} alt="" /> </td>
+                  <td>{user.name.first}</td>
+                  <td>{user.name.last}</td>
+                  <td>{user.location.country}</td>
+                  <td>accion</td>
+
+                </tr>)
+            }
+            )}
+
+
+          </tbody>
+
+        </table>
+      </div >
+    </>
   );
 }
 
