f71ede8c8008ce230456d4875cf12660ed65018f
jeshua123 <inv.estevez@gmail.com>
2023-10-12 04:21:28 -0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch("https://randomuser.me/")
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +23,16 @@ function App() {
             <th>Pais</th>
             <th>Acciones</th>
           </tr>
+          <tr>
+            <td>imagen </td>
+            <td>juan</td>
+            <td>perez</td>
+            <td>chile</td>
+            <td>5</td>
+
+          </tr>
         </tbody>
+
       </table>
     </div >
   );
