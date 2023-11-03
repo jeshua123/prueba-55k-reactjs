8a5a40c4c589163b271e850ee7efe3f954e9bba9
jeshua123 <inv.estevez@gmail.com>
2023-11-03 05:32:35 -0300

    03-11-2023

--- src/App.js
+++ src/App.js
@@ -1,96 +1,81 @@
-import { useEffect, useState, useRef, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
 
+
+function App() {
   const [users, setUsers] = useState([]);
-  const [showRowColors, setshowRowColors] = useState(false);
-  const [showSortUsers, setShowSortUsers] = useState(false);
-  const [filterCountry, setFilterCountry] = useState(null);
-  const ref = useRef([])
+
   useEffect(() => {
-    fetch('https://randomuser.me/api/?results=100')
-      .then(res => res.json())
-      .then(res => {
-        setUsers(res.results)
-        ref.current = res.results
+
+    fetch("https://randomuser.me/api/?results=100")
+      .then(response => response.json())
+      .then(data => {
+
+        setUsers(data.results)
 
       })
-  }, [])
-
-  const activateRowcolors = () => { setshowRowColors(!showRowColors) }
-
-  const sortingUsers = () => { setShowSortUsers(!showSortUsers) }
-
-  const filteredUsers = useMemo(() => {
-    return filterCountry ? users.filter(user => user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
-    ) : users
-  }, [users, filterCountry])
-
-  const sortedUsers = useMemo(() => {
-    return showSortUsers ?
-      filteredUsers.toSorted((a, b) => { return a.location.country.localeCompare(b.location.country) })
-      : filteredUsers
-  }, [filteredUsers, showSortUsers])
-
-
-  const sortButtonInfoDisplay = showSortUsers ? "no ordenar por pais" : "ordenar por pais"
-
-  const handleDeleted = (email) => { setUsers(users.filter((users) => users.email !== email)) }
-
-  const setStated = () => {
-    setUsers(ref.current)
-  }
-
+  }, []);
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <h1>Prueba tecnica</h1>
-          <nav>
-            <button onClick={activateRowcolors} >Colorear Files</button>
-            <button onClick={sortingUsers}>{sortButtonInfoDisplay}</button>
-            <button onClick={setStated}>resetear estado</button>
-            <input type="text" name="" id="" placeholder='filtrar por pais ' onChange={(e) => {
-              setFilterCountry(e.target.value)
-            }
-            } />
-          </nav>
-        </header>
+
+      <header>
+        <h1>Prueba tecnica</h1>
+      </header>
+      <nav>
+
+        <button>Colorear Files</button>
+        <button>Ordenar por pais</button>
+        <button>Resetar Estado</button>
+        <input type="text" name="" id="" />
+
+      </nav>
+      <section>
+
         <table>
-
-          <tbody>
-            <tr >
-              <th>Foto</th>
-              <th>Nombre</th>
-              <th>Apellido</th>
-              <th>Pais</th>
-              <th>Accion</th>
+          <thead>
+            <tr>
+              <th>
+                Foto
+              </th>
+              <th>
+                Nombre
+              </th>
+              <th>
+                Apellido
+              </th>
+              <th>
+                Pais
+              </th>
+              <th>
+                Acciones
+              </th>
             </tr>
+          </thead>
+          <tbody>
 
-            {sortedUsers.map((user, index) => {
-
-              const rowColor = showRowColors ? index % 2 === 0 ? "bgc-1" : "bgc-2" : null
-
+            {users.map((user) => {
               return (
-                <tr className={rowColor} key={index}>
-                  <td><img src={user.picture.thumbnail} alt="" /> </td>
-                  <td>{user.name.first}</td>
-                  <td>{user.name.last}</td>
-                  <td>{user.location.country}</td>
-                  <td onClick={() => { handleDeleted(user.email) }}>Borrar</td>
+                <>
+                  <tr>
+                    <td><img src={user.picture.thumbnail} alt="" /></td>
+                    <td>{user.name.first}</td>
+                    <td>{user.name.last}</td>
+                    <td>{user.location.country}</td>
+                    <td>Borrar</td>
 
-                </tr>)
-            }
-            )}
+                  </tr>
+                </>
+              )
+            })}
 
 
           </tbody>
-
         </table>
-      </div >
+
+      </section>
     </>
-  );
+  )
 }
 
 export default App;
