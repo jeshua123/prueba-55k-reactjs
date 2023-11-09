d85b5ea097caf1d4e363d65638865a9b062c5575
jeshua123 <inv.estevez@gmail.com>
2023-11-09 06:11:24 -0300

    9-11-2023

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
-
-
 function App() {
+
   const [users, setUsers] = useState([]);
+
+  const [sortedUsers, setSortedUsers] = useState([]);
+
+  const [changeColorShow, setChangeColorShow] = useState();
+  const [activatedSorting, setActivatedSorting] = useState(false);
+  const usersRef = useRef([])
 
   useEffect(() => {
 
     fetch("https://randomuser.me/api/?results=100")
       .then(response => response.json())
       .then(data => {
-
         setUsers(data.results)
-
+        usersRef.current = data.results
       })
   }, []);
+
+  function changeColorsOnClick() {
+    setChangeColorShow(!changeColorShow)
+  }
+
+  const sortingUsersByCountry = () => {
+
+    setSortedUsers(users.toSorted((a, b) => { return a.location.country.toLowerCase().localeCompare(b.location.country.toLowerCase()) }))
+    setActivatedSorting(!activatedSorting)
+
+  }
+
+  const deleteRow = (params) => {
+
+  }
+
+
+  const UsersDisplayedArray = activatedSorting ? sortedUsers : users
+
+  const sortingButtonText = activatedSorting ? "Ordenar por Pais" : "No Ordenar por Pais"
   return (
     <>
-
       <header>
         <h1>Prueba tecnica</h1>
       </header>
       <nav>
-
-        <button>Colorear Files</button>
-        <button>Ordenar por pais</button>
+        <button onClick={() => {
+          changeColorsOnClick()
+        }
+        }>Colorear Files</button>
+        <button onClick={() => {
+          sortingUsersByCountry()
+        }
+        } >{sortingButtonText}</button>
         <button>Resetar Estado</button>
         <input type="text" name="" id="" />
-
       </nav>
       <section>
 
@@ -54,16 +81,22 @@ function App() {
           </thead>
           <tbody>
 
-            {users.map((user) => {
+            {UsersDisplayedArray.map((user, index) => {
+
+              const displayBackGroundColor = changeColorShow ? index % 2 > 0 ? "bgc-1" : "bgc-2" : null
+
               return (
                 <>
-                  <tr>
+                  <tr className={displayBackGroundColor}>
                     <td><img src={user.picture.thumbnail} alt="" /></td>
                     <td>{user.name.first}</td>
                     <td>{user.name.last}</td>
                     <td>{user.location.country}</td>
-                    <td>Borrar</td>
-                  </tr>
+                    <td onClick={() => {
+                      deleteRow()
+                    }
+                    } >Borrar</td>
+                  </tr >
                 </>
               )
             })}
@@ -72,7 +105,7 @@ function App() {
           </tbody>
         </table>
 
-      </section>
+      </section >
     </>
   )
 }
