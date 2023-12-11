f42c2bbdb30a6be2ad1fedcebb36ab25ffd629f0
jeshua123 <inv.estevez@gmail.com>
2023-12-11 10:27:50 -0300

    11-12-2023

--- src/App.js
+++ src/App.js
@@ -3,121 +3,121 @@ import './App.css';
 
 function App() {
 
-  const [users, setUsers] = useState([]);
-  const [changeColorShow, setChangeColorShow] = useState();
-  const [activatedSorting, setActivatedSorting] = useState(false);
-  const usersRef = useRef([])
+   const [users, setUsers] = useState([]);
+   const [changeColorShow, setChangeColorShow] = useState();
+   const [activatedSorting, setActivatedSorting] = useState(false);
+   const usersRef = useRef([])
+   useEffect(() => {
 
-  useEffect(() => {
+      fetch("https://randomuser.me/api/?results=100")
+         .then(response => response.json())
+         .then(data => {
+            setUsers(data.results)
+            usersRef.current = data.results
+         })
+   }, []);
 
-    fetch("https://randomuser.me/api/?results=100")
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data.results)
-        usersRef.current = data.results
-      })
-  }, []);
+   function changeColorsOnClick() {
+      setChangeColorShow(!changeColorShow)
+   }
+   const sortedUsers = users.toSorted((a, b) => { return a.location.country.toLowerCase().localeCompare(b.location.country.toLowerCase()) })
+   const sortingUsersByCountry = () => {
 
-  function changeColorsOnClick() {
-    setChangeColorShow(!changeColorShow)
-  }
-  const sortingUsersByCountry = () => {
-    setUsers(users.toSorted((a, b) => { return a.location.country.toLowerCase().localeCompare(b.location.country.toLowerCase()) }))
-    setActivatedSorting(!activatedSorting)
-  }
+      setActivatedSorting(!activatedSorting)
+   }
 
-  const setBackToOriginalState = () => {
-    setUsers(usersRef.current)
+   const setBackToOriginalState = () => {
+      setUsers(usersRef.current)
 
-  }
+   }
 
-  const UsersDisplayedArray = activatedSorting ? users : usersRef.current
+   const displayedArray = activatedSorting ? users : sortedUsers
+   const sortingButtonText = activatedSorting ? "No Ordenar por Pais" : "Ordenar por Pais"
 
-  const sortingButtonText = activatedSorting ? "No Ordenar por Pais" : "Ordenar por Pais"
+   const deleteRow = (uuid) => {
+      setUsers(users.filter(user => user.login.uuid !== uuid))
 
-  const deleteRow = (uuid) => {
-    setUsers(UsersDisplayedArray.filter(user => user.login.uuid !== uuid))
-  }
-  const filterByCountry = (e) => {
-
-
-
-  }
-
-  return (
-    <>
-      <header>
-        <h1>Prueba tecnica</h1>
-      </header>
-      <nav>
-        <button onClick={() => {
-          changeColorsOnClick()
-        }
-        }>Colorear Files</button>
-        <button onClick={() => {
-          sortingUsersByCountry()
-        }
-        } >{sortingButtonText}</button>
-        <button onClick={() => {
-          setBackToOriginalState()
-        }
-        }>Resetar Estado</button>
-        <input onChange={(e) => {
-          filterByCountry(e)
-        }
-        } type="text" name="" id="" />
-      </nav>
-      <section>
-
-        <table>
-          <thead>
-            <tr>
-              <th>
-                Foto
-              </th>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Apellido
-              </th>
-              <th>
-                Pais
-              </th>
-              <th>
-                Acciones
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-
-            {UsersDisplayedArray.map((user, index) => {
-
-              const displayBackGroundColor = changeColorShow ? index % 2 > 0 ? "bgc-1" : "bgc-2" : null
-
-              return (
-                <>
-                  <tr className={displayBackGroundColor}>
-                    <td><img src={user.picture.thumbnail} alt="" /></td>
-                    <td>{user.name.first}</td>
-                    <td>{user.name.last}</td>
-                    <td>{user.location.country}</td>
-                    <td onClick={() => {
-                      deleteRow(user.login.uuid)
-                    }
-                    } >Borrar</td>
-                  </tr >
-                </>
-              )
-            })}
+   }
+   const filterByCountry = (e) => {
 
 
-          </tbody>
-        </table>
+      setUsers(users.filter(user => user.location.country === e.target.value))
+   }
 
-      </section >
-    </>
-  )
+   return (
+      <>
+         <header>
+            <h1>Prueba tecnica</h1>
+         </header>
+         <nav>
+            <button onClick={() => {
+               changeColorsOnClick()
+            }
+            }>Colorear Files</button>
+            <button onClick={() => {
+               sortingUsersByCountry()
+            }
+            } >{sortingButtonText}</button>
+            <button onClick={() => {
+               setBackToOriginalState()
+            }
+            }>Resetar Estado</button>
+            <input onChange={(e) => {
+               filterByCountry(e)
+            }
+            } type="text" name="" id="" />
+         </nav>
+         <section>
+
+            <table>
+               <thead>
+                  <tr>
+                     <th>
+                        Foto
+                     </th>
+                     <th>
+                        Nombre
+                     </th>
+                     <th>
+                        Apellido
+                     </th>
+                     <th>
+                        Pais
+                     </th>
+                     <th>
+                        Acciones
+                     </th>
+                  </tr>
+               </thead>
+               <tbody>
+
+                  {displayedArray.map((user, index) => {
+
+                     const displayBackGroundColor = changeColorShow ? index % 2 > 0 ? "bgc-1" : "bgc-2" : null
+
+                     return (
+                        <>
+                           <tr key={index} className={displayBackGroundColor}>
+                              <td><img src={user.picture.thumbnail} alt="" /></td>
+                              <td>{user.name.first}</td>
+                              <td>{user.name.last}</td>
+                              <td>{user.location.country}</td>
+                              <td onClick={() => {
+                                 deleteRow(user.login.uuid)
+                              }
+                              } >Borrar</td>
+                           </tr >
+                        </>
+                     )
+                  })}
+
+
+               </tbody>
+            </table>
+
+         </section >
+      </>
+   )
 }
 
 export default App;
